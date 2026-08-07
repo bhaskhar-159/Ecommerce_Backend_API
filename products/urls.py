f8de9a58b3e2117b3f32547e47dbaeafd90fbfee
bhaskhar-159 <bhaskarma15191@gmail.com>
2026-08-07 14:14:51 +0530

    Created Product  CRUD API

--- products/urls.py
+++ products/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path
-from .views import CategoryListAPIView
+from .views import CategoryListAPIView, CategoryDetailAPIView,  ProductListAPIView, ProductDetailAPIView
 
 urlpatterns = [
     path("categories/", CategoryListAPIView.as_view(), name="category-list"),
+    path("categories/<int:pk>/", CategoryDetailAPIView.as_view(), name="category-detail"),
+    path("products/", ProductListAPIView.as_view(), name="product-list"),
+    path("products/<int:pk>/", ProductDetailAPIView.as_view(), name="product-detail"),
 ]
--- products/views.py
+++ products/views.py
@@ -1,9 +1,16 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
 
-from .models import Category
-from .serializers import CategorySerializer
+# What happens if someone requests:/api/v1/categories/999/ and category 999 doesn't exist?
+# Django will raise: Category.DoesNotExist and the API can return a server error.
+    # So we use get_object_or_404
 
+from django.shortcuts import get_object_or_404
+
+from .models import Category, Product
+from .serializers import CategorySerializer, ProductSerializer
+
+# Category CRUD API
 
 class CategoryListAPIView(APIView):
 
@@ -11,3 +18,113 @@ class CategoryListAPIView(APIView):
         categories = Category.objects.all()
         serializer = CategorySerializer(categories, many=True)
         return Response(serializer.data)
+    
+   
+    def post(self, request):
+        serializer = CategorySerializer(data=request.data)
+
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=201)
+
+        return Response(serializer.errors, status=400)
+    
+class CategoryDetailAPIView(APIView):
+
+    def get(self, request, pk):
+        category = get_object_or_404(Category, pk=pk)
+        serializer = CategorySerializer(category)
+        return Response(serializer.data)    
+    
+
+    def put(self, request, pk):
+        category = get_object_or_404(Category, pk=pk)
+
+        serializer = CategorySerializer(category, data=request.data)
+
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+
+        return Response(serializer.errors, status=400)    
+
+
+    def patch(self, request, pk):
+        category = get_object_or_404(Category, pk=pk)
+
+        serializer = CategorySerializer(category, data=request.data, partial=True)
+
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+
+        return Response(serializer.errors, status=400)
+
+    def delete(self, request, pk):
+        category = get_object_or_404(Category, pk=pk)
+
+        category.delete()
+
+        return Response(status=204)
+    
+# Product CRUD API   
+    
+class ProductListAPIView(APIView):
+
+    def get(self, request):
+        products = Product.objects.all()
+
+        serializer = ProductSerializer(products, many=True)
+
+        return Response(serializer.data)
+
+    def post(self, request):
+        serializer = ProductSerializer(data=request.data)
+
+        if serializer.is_valid():
+            serializer.save()
+
+            return Response(serializer.data, status=201)
+
+        return Response(serializer.errors, status=400)    
+    
+    
+class ProductDetailAPIView(APIView):
+
+    def get(self, request, pk):
+        product = get_object_or_404(Product, pk=pk)
+
+        serializer = ProductSerializer(product)
+
+        return Response(serializer.data)
+
+    def put(self, request, pk):
+        product = get_object_or_404(Product, pk=pk)
+
+        serializer = ProductSerializer(product, data=request.data)
+
+        if serializer.is_valid():
+            serializer.save()
+
+            return Response(serializer.data)
+
+        return Response(serializer.errors, status=400)
+
+    def patch(self, request, pk):
+        product = get_object_or_404(Product, pk=pk)
+
+        serializer = ProductSerializer(product, data=request.data, partial=True)
+
+        if serializer.is_valid():
+            serializer.save()
+
+            return Response(serializer.data)
+
+        return Response(serializer.errors, status=400)
+
+    def delete(self, request, pk):
+        product = get_object_or_404(Product, pk=pk)
+
+        product.delete()
+
+        return Response(status=204)    
